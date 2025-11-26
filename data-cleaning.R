e69b5058492a4edaf8f2fc3ef8cfe217e22ce729
emh-99 <emilyhou1gmail.com>
2025-11-26 00:04:16 -0800

    progress on LVCF for outcome

--- data-cleaning.R
+++ data-cleaning.R
@@ -205,4 +205,88 @@ for (i in 3:length(all_icpsr)) {
   clean_df <- merge(clean_df, visit_i_df, by="SWANID", all.x=T)
 }
 
+###################################
+
+which(colnames(clean_df) == "HPBMDT0") #everything up to the 17th col is baseline covariates
+
+for (i in 1:17){
+  print(any(is.na(clean_df[,i])))
+} #no one is missing its baseline covariates, so we don't need the delta nodes for that
+
+#dropped the age variables after the baseline 
+clean_df <- clean_df %>% select(-grep("AGE", names(clean_df), value=TRUE)[-1])
+
+##### LVCF!! for the intermediate covariates #####
+
+#fixing some column names
+clean_df <- rename(clean_df, DIABETE0 = DIABETE)
+clean_df <- rename(clean_df, MARITAL0 = MARITALGP)
+names(clean_df) <- sub("INSULN\\d?", "INSULIN", names(clean_df)) #fixed insuln to insulin and gets rid of the extra digit
+
+#making the level names of all the status columns the same (they are the same thing just written differently and that is messing with things)
+status_cols <- grep("STATUS", names(clean_df), value = TRUE)[-1]
+for (col in status_cols){
+  levels(clean_df[[col]]) <- levels(clean_df$STATUS0)
+}
+
+#vector with all the col names for the intermediate covariates
+int_cov <- grep("HORMUSER|SPBMDT|HPBMDT|CSPINE|CHIP", names(clean_df)[-c(1:17)], value = TRUE, invert = TRUE)
+
+#actually doing LVCF
+for (cur_col in int_cov){
+  i <- as.numeric(regmatches(cur_col, regexpr('\\d+', cur_col))) #extracts the number at the end of the column name
+  prev_col <- paste(regmatches(cur_col, regexpr('[[:alpha:]]+', cur_col)), i-1, sep = "") #gets the name of the column from the previous visit
+  ind <- which(is.na(clean_df[cur_col])) #index of which values are NA and need to get replaced
+  clean_df[cur_col][ind,] <- clean_df[prev_col][ind,] #replaces NAs with value from previous visit
+}
+
+any(is.na(clean_df[int_cov])) #yay 
+
+##### adding delta A columns, indicator of whether the exposure was measured ###
+treat_cols <- grep("HORMUSER", names(clean_df), value = TRUE)
+
+delta_cols <- sub("USER", "DELTA", treat_cols)
+for (i in seq_along(treat_cols)) {
+  clean_df <- clean_df %>%
+    mutate(
+      !!delta_cols[i] := ifelse(is.na(.data[[treat_cols[i]]]), 0, 1)) %>% #makes the delta col
+    relocate(
+      !!sym(delta_cols[i]), .after = !!sym(treat_cols[i])) #moves it right after the corresponding treatment col
+}
+
+##### make baseline diabete into 0/1 
+no_diabetes <- levels(clean_df$DIABETE0)[1]
+clean_df$DIABETE0 <- ifelse(clean_df$DIABETE0 == no_diabetes, 0, 1)
+
+##### LVCF for the outcome. we only want to censor people who are missing the outcome at visit 10. 
+#missing inbetween values will be imputed using LVCF.
+
+#first, let's check if there are people who have an outcome at timepoint 10 who are censored sometime before that
+#spine
+cspine_cols <- clean_df %>% select(grep("CSPINE", names(clean_df), value = T))
+cspine_cols %>% filter(CSPINE10 == 1, rowSums(cspine_cols) != 11) #yes, there are many.
+which(cspine_cols$CSPINE10 ==1 & rowSums(cspine_cols) != 11)
+
+#hip
+chip_cols <- clean_df %>% select(grep("CHIP", names(clean_df), value = T))
+chip_cols %>% filter(CHIP10 == 1, rowSums(chip_cols) != 11) #huh there might be less rows than with the spine?
+
+#now we will do LVCF on the SPBMDT (spine outcome) columns 
+spine_cols <- grep("SPBMDT", names(clean_df), value = TRUE)[-1]
+test_df <- clean_df
+for (cur_col in spine_cols){
+  i <- as.numeric(regmatches(cur_col, regexpr('\\d+', cur_col))) #extracts the number at the end of the column name
+  prev_col <- paste(regmatches(cur_col, regexpr('[[:alpha:]]+', cur_col)), i-1, sep = "") #gets the name of the column from the previous visit
+  ind <- which(is.na(clean_df[cur_col]) & !is.na(clean_df$SPBMDT10)) #index of which values are NA and need to get replaced, but only if the outcome at the last timepoint is not missing
+  test_df[cur_col][ind,] <- clean_df[prev_col][ind,] #replaces NAs with value from previous visit
+}
+
+View(test_df)
+
+
+View(clean_df %>% select(grep("SPBMDT", names(clean_df), value = TRUE)))
+View(test_df %>% select(grep("SPBMDT", names(clean_df), value = TRUE)))
+## 11/26 notes, need to fix LVCF code to handle two NAs in a row properly, then do the hip columns, then do one-hot encoding
+
 write.csv(clean_df, here("data", "clean_data.csv"), row.names=F)
+
