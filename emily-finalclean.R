eb88e2e9124da486bd115ad42759ec364b2f170d
emh-99 <emilyhou1gmail.com>
2025-12-02 20:39:21 -0800

    ran ltmle

--- emily-finalclean.R
+++ emily-finalclean.R
@@ -1,24 +1,32 @@
-
 library(here)
 library(dplyr)
 library(tidyverse)
 library(data.table)
 
 #read data
-final_df <- read.csv("both_outcome_w_shift.csv")
+final_df <- read.csv(here("data", "both_outcome_w_shift.csv"), header=TRUE)
 
 #remove c_hip cols
 final_df <- final_df %>% select((-starts_with("C_HPBMDT")))
 #remove delta_a cols
 final_df <- final_df %>% select((-starts_with("D_HORMUSER")))
 
+#because we doing ltmle on hip with spine as a covariate, the spine cols should come before the A, C, Y
+k <- 3   #number of columns to move forward
+cols <- names(final_df)
+hip_ind <- grep("^HPBMDT", cols) #positions of hip columns
+new_order <- seq_along(cols)
+for (i in hip_ind) {
+  old_pos <- which(new_order == i)
+  new_pos <- min(old_pos - k, length(cols)) #to not go off the end lol
+  new_order <- append(new_order[-old_pos], old_pos, after = new_pos - 1) #remove from old_pos and reinsert at new_pos
+}
+final_df <- final_df[, new_order]
+
 #fix censoring for c_spine --going to just make everything after the first appearance of censoring to be NA
 c_spine_cols <- grep("C_SPBMDT", names(clean_df), value = TRUE)
-
 all_cols <- names(final_df)
-test_df <- final_df[1:25,]
-
-test_df <- test_df %>%
+final_df <- final_df %>%
   rowwise() %>%
   mutate(
     pos_small = match(1, c_across(all_of(c_spine_cols))), #position of the column with the first censor in c_spine_cols
@@ -35,6 +43,6 @@ test_df <- test_df %>%
       })
   ) %>%ungroup() %>% select(-pos_small, -pos_big) #get rid of added position columns
 
-View(test_df)
-         
+#View(final_df)
 
+write.csv(final_df, here("data", "final_df.csv"), row.names=FALSE)
--- ltmle.R
+++ ltmle.R
@@ -5,25 +5,42 @@ library(dplyr)
 library(tidyverse)
 library(data.table)
 
-data <- read.csv(here("data", "clean_data.csv"), header=T)
-View(clean_df[29,])
-colnames(clean_df) %>% sort
+final_df <- read.csv(here("data", "final_df.csv"), header=TRUE)
+final_df <- final_df %>% select(-SWANID)
 
-View(clean_df)
+cols <- colnames(final_df)
+Anodes <- grep("HORMUSER", cols, value=TRUE)
+Cnodes <- sapply(1:10, function(i) paste0("C_SPBMDT", i))
+Ynodes <- sapply(0:10, function(i) paste0("SPBMDT", i))
+Lnodes <- cols[!cols %in% Anodes & !cols %in% Ynodes & !cols %in% Cnodes]
+Lnodes <- Lnodes[-c(1:which(Lnodes == "HPBMDT0"))]
 
-cols <- grep("HORMUSER", names(clean_df), value = TRUE)
+for (col in Cnodes) {
+  final_df[col] <- BinaryToCensoring(is.censored = final_df[col])
+}
 
-# Find which rows have at least one NA but not all NA among those columns
-rows_with_some_na <- which(
-  apply(is.na(clean_df[cols]), 1, any) &      # at least one NA
-    !apply(is.na(clean_df[cols]), 1, all)       # not all NA
-)
+abar1 <- rep(1, length(Anodes))
+abar0 <- rep(0, length(Anodes))
+bound <- 5/sqrt(nrow(final_df))/log(nrow(final_df))
 
-clean_df[rows_with_some_na, cols] %>% View()
+baselinefit <- ltmle(data = final_df,
+              Anodes = Anodes,
+              Cnodes = Cnodes,
+              Lnodes = Lnodes,
+              Ynodes = Ynodes,
+              abar = list(abar1, abar0),
+              gbounds = c(bound, 1-bound),
+              SL.library = list("SL.glm", "SL.mean", c("SL.glm", "screen.corP")))
 
-which(apply(is.na(clean_df[cols]), 1, any))
+summary(baselinefit)
+hist(baselinefit$IC[,1])
+baselinefit$fit$Q[[2]]
 
+#Additive Treatment Effect:
+#Parameter Estimate:  0.06231 
+#Estimated Std Err:  0.011828 
+#p-value:  1.3777e-07 
+#95% Conf Interval: (0.039129, 0.085492) 
 
-
-
+#influence curve looks ok i think!
 
