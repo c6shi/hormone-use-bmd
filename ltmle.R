f7a96d413cb15f6b954d41c0069a2e2492c166ac
Emily Hou <emilyhou1@gmail.com>
2025-12-03 13:53:18 -0800

    ran more ltmle

--- ltmle.R
+++ ltmle.R
@@ -4,6 +4,12 @@ library(here)
 library(dplyr)
 library(tidyverse)
 library(data.table)
+library(SuperLearner)
+library(glmnet)
+library(earth)
+#install.packages("SuperLearner")
+#install.packages("glmnet")
+install.packages("earth")
 
 final_df <- read.csv(here("data", "final_df.csv"), header=TRUE)
 final_df <- final_df %>% select(-SWANID)
@@ -44,3 +50,39 @@ baselinefit$fit$Q[[2]]
 
 #influence curve looks ok i think!
 
+fit2 <- ltmle(data = final_df,
+              Anodes = Anodes,
+              Cnodes = Cnodes,
+              Lnodes = Lnodes,
+              Ynodes = Ynodes,
+              abar = list(abar1, abar0),
+              gbounds = c(bound, 1-bound),
+              SL.library = list("SL.glm", "SL.glmnet", "SL.earth", 
+                                c("SL.glm", "screen.corP"), c("SL.glmnet", "screen.corP"),
+                                c("SL.earth", "screen.corP")))
+
+summary(fit2)
+#Additive Treatment Effect:
+#Parameter Estimate:  0.064189 
+#Estimated Std Err:  0.011008 
+#p-value:  5.5009e-09 
+#95% Conf Interval: (0.042614, 0.085763) 
+
+hist(fit2$IC[,1])
+fit2$fit$Q[[1]] #after looking at this, gonna remove glmnet and add something else??
+
+#being ambitious
+fit3 <- ltmle(data = final_df,
+              Anodes = Anodes,
+              Cnodes = Cnodes,
+              Lnodes = Lnodes,
+              Ynodes = Ynodes,
+              abar = list(abar1, abar0),
+              gbounds = c(bound, 1-bound),
+              SL.library <- list(
+                "SL.glm", "SL.glm.interaction", "SL.earth", "SL.ranger",
+                c("SL.glm", "screen.corP"),
+                c("SL.glm.interaction", "screen.corP"),
+                c("SL.earth", "screen.corP"),
+                c("SL.ranger", "screen.corP")))
+              
